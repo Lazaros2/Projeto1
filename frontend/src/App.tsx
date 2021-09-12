a89e93c10114430de76b589b12decc34e4c4a048
Lazaro <laznas@alunos.utfpr.edu.br>
2021-09-12 15:18:11 -0300

    Gráficos estáticos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,39 @@
 import DataTable from "components/datatable";
 import NavBar from "components/NavBar";
 import Footer from "components/footer";
+import BarChart from "components/barchart";
+import DonutChart from "components/DonnutChart";
 
 
 function App() { //
   return (
     <>
-    <NavBar/>
-    <div className= "container">
-      <h1 className="text-primary">Ola mundo</h1>
-      
-      <DataTable />
-     
-    </div>
-     <Footer />
-     </>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary py-3 text">Dashboard de vendas</h1>
+
+
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h4 className="text-center text-secondary">
+              Taxa de sucesso(%)
+            </h4>
+            <BarChart />
+          </div>
+          <div className="col-sm-6">
+            <h4 className="text-center text-secondary">
+              Taxa de sucesso(%)
+            </h4>
+            <DonutChart />
+          </div>
+        </div>
+        <div className="py-3">
+          <h2 className="text-primary"></h2>
+        </div>
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
