60ba34d4332a70401d698b4cec96aedb17a75502
Lazaro <laznas@alunos.utfpr.edu.br>
2021-09-12 13:25:46 -0300

    Componentes estáticos inseridos

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,21 @@
+import DataTable from "components/datatable";
+import NavBar from "components/NavBar";
+import Footer from "components/footer";
 
 
-function App() {
+function App() { //
   return (
-    <div>
+    <>
+    <NavBar/>
+    <div className= "container">
       <h1 className="text-primary">Ola mundo</h1>
+      
+      <DataTable />
+     
     </div>
+     <Footer />
+     </>
   );
 }
 
-export default App;
+export default App; // o que vai ficar visível(tipo um public)
